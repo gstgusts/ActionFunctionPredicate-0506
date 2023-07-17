fca890616ff75aaa8e352d5aebfb9ab9c1faffdd
Gusts Linkevics <gusts@gst.lv>
2023-07-17 19:30:01 +0300

    Add project files.

---
+++ ActionFunctionPredicate-0506/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ActionFunctionPredicate_0506
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> log = new Action<string>(LogInfo);
+            log.Invoke("Hi ALL");
+            Console.ReadLine();
+
+        }
+
+        static void LogInfo(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
